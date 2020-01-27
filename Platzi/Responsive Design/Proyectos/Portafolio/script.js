ae38558111c8a0b7ebdf89b0f58f8c363beb8602
DCueto <dcueto14.info@gmail.com>
2020-01-27 21:09:36 +0100

    Menú desplegable interactivo (JS)

---
+++ Platzi/Responsive Design/Proyectos/Portafolio/script.js
@@ -0,0 +1,16 @@
+
+// console.log(document.querySelector('.menu'));
+const $menu = document.querySelector('.menu');
+const $burgerButton = document.querySelector("#burger-menu");
+
+console.log($burgerButton);
+
+$burgerButton.addEventListener("click", hideShow);
+
+function hideShow(){
+  if($menu.classList.contains('is-active')){
+    $menu.classList.remove('is-active');
+  } else {
+    $menu.classList.add('is-active');
+  }
+}
