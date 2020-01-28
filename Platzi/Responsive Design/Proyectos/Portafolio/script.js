41c700c252e6fef1f0aa0ff395c8771de3f2bba6
DCueto <dcueto14.info@gmail.com>
2020-01-28 01:04:32 +0100

    Media Query validated with fixed loaded resolutions (burger menu fails on iPhone 6s)

--- Platzi/Responsive Design/Proyectos/Portafolio/script.js
+++ Platzi/Responsive Design/Proyectos/Portafolio/script.js
@@ -5,9 +5,15 @@ const $burgerButton = document.querySelector("#burger-menu");
 const $mQueryipad = window.matchMedia('screen and (max-width: 767px');
 // console.log($burgerButton);
 
+// if ($mQueryipad.matches){
+//   $burgerButton.addEventListener("click", hideShow);
+// } else {
+//   $burgerButton.removeEventListener("click", hideShow);
+// }
+
+console.log($mQueryipad);
 
 $mQueryipad.addListener(validation);
-
 
 function validation(event){
   if (event.matches){
@@ -18,6 +24,8 @@ function validation(event){
   console.log(event.matches);
 }
 
+validation($mQueryipad);
+
 function hideShow(){
   if($menu.classList.contains('is-active')){
     $menu.classList.remove('is-active');
@@ -25,3 +33,4 @@ function hideShow(){
     $menu.classList.add('is-active');
   }
 }
+
