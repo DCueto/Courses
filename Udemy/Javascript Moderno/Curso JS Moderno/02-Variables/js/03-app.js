be8a8d9ccf6d2607346161bbc107f93f01385fb1
DCueto <dcueto14.info@gmail.com>
2021-01-23 19:30:08 +0100

    Declarando variables con const

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,16 @@
+// Las variables con const son CONSTANTES
+
+const producto = "Tablet";
+
+// No se pueden reasignar variables con const
+// producto = "Monitor";
+
+console.log(producto);
+
+// No se puede inicializar una constante sin valor
+// const precio;
+// precio = 20;
+// console.log(precio);
+
+const precio = 20;
+console.log(precio);
