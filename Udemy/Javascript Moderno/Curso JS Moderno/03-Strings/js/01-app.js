a9f7f1ffdd124fe5a29facbe6a43cb263d7d201b
DCueto <dcueto14.info@gmail.com>
2021-01-23 20:02:47 +0100

    Crear String y usar String Methods

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/03-Strings/js/01-app.js
@@ -0,0 +1,10 @@
+// Formas de crear strings
+
+const producto = "Monitor de 20 \"";
+console.log(producto);
+
+const producto2 = String('Monitor 24 "');
+console.log(producto2);
+
+const producto3 = new String('Monitor de 27 pulgadas');
+console.log(producto3);
