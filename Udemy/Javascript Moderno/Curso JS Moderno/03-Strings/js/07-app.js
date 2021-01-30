71a3ebb660a52799d476b8c15a4770e7271415b9
DCueto <dcueto14.info@gmail.com>
2021-01-30 21:03:32 +0100

    String Methods: convertir a Mayúsculas y Minúsuculas. Cambiar un número a string.

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/03-Strings/js/07-app.js
@@ -0,0 +1,25 @@
+
+const producto = 'Monitor 20 Pulgadas';
+
+
+// Convertir a mayúsculas una cadena de texto
+console.log(producto.toUpperCase());
+
+
+// Convertir a minúsculas una cadena de texto;
+console.log(producto.toLowerCase());
+
+
+// Ejemplo
+// A veces la gente introduce mayúsuclas en el campo de mail y puede crear conflictos en la base de datos
+// así que lo mejor es siempre convertir en minúsculas los correos.
+const email = "CORREO@CORREO.COM";
+const email2 = "Juan@correo.com";
+
+console.log(email.toLowerCase());
+console.log(email2.toLowerCase());
+
+
+// Convertir un número a string
+const precio = 300;
+console.log(precio.toString());
