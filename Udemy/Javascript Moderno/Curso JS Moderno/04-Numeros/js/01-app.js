5a21f5f1e3fd55c4aa490f3622400eec1b192dce
DCueto <dcueto14.info@gmail.com>
2021-03-23 17:00:48 +0100

    Números y operaciones básicas

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/04-Numeros/js/01-app.js
@@ -0,0 +1,27 @@
+
+// Creación de números
+
+// En la consola el número sale en color AZUL y el string sale en color NEGRO.
+const numero1 = 20;
+const numero2 = "20";
+
+console.log(numero1); // Número
+console.log(numero2); // String
+
+const numero_flotante = 20.20; // Num decimal
+const numero_flotante2 = .15614666; // Número decimal (0.1561...)
+
+
+console.log(numero_flotante);
+console.log(numero_flotante2);
+
+const numero_negativo = -20; // Número negativo
+
+console.log(numero_negativo);
+
+
+// Otra forma de crear un número
+
+// Esto crea un objeto de tipo número, así que nos imprime el objeto como tal.
+const numero_new = new Number(80);
+console.log(numero_new);
