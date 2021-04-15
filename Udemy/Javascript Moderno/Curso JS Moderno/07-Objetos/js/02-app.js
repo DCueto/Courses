0fcd01fdd9fd33bf846377918789658004ab6955
DCueto <dcueto14.info@gmail.com>
2021-04-15 14:02:33 +0200

    Acceder a valores del objeto

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/07-Objetos/js/02-app.js
@@ -0,0 +1,16 @@
+
+// Object literal
+const producto = {
+  nombre: "Monitor de 20 Pulgadas",
+  precio: 300,
+  disponible: true,
+}
+
+console.log(producto);
+
+// Acceder a los valores de un objecto
+// Con punto
+console.log(producto.nombre);
+
+// Con corchetes
+console.log(producto['precio']);
