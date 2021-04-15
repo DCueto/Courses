3b4a572b0cddc947be5c0e5e3e809f68c91c2a45
DCueto <dcueto14.info@gmail.com>
2021-04-15 13:30:10 +0200

    Creando primer objeto

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/07-Objetos/js/01-app.js
@@ -0,0 +1,23 @@
+
+// Los objetos son la pieza principal en Javascript
+
+const nombre = "Monitor de 20 pulgadas";
+const precio = 300;
+const disponible = true;
+
+// En vez de usar tres variables podemos usar un objecto con tres propiedades:valor
+
+// Un objeto agrupa todo en una sola variable
+
+// Object literal
+const producto = {
+  // propiedad: valor
+  // en los objetos se separan las propiedades:valor con , no con ;
+  // si lo separas con ; te saltará un error
+  nombre: "Monitor de 20 Pulgadas",
+  precio: 300,
+  disponible: true,
+}
+
+console.log(producto);
+
