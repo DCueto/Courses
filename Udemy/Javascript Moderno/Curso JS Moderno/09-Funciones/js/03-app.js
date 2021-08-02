0045bc0c51d552623fb335ea6dffa02857fc8e04
DCueto <dcueto14.info@gmail.com>
2021-08-02 18:24:00 +0200

    funciones nativas de JS

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/09-Funciones/js/03-app.js
@@ -0,0 +1,13 @@
+// Funciones nativas de JS (core de JS)
+
+// Alerta
+alert("Hubo un error...");
+
+// Alerta con campo
+prompt("¿Cual es tu edad?");
+
+// Parsear un string a entero
+console.log(parseInt("20"));
+
+
+// Hay muchísimas funciones más, estas solo son algunos ejemplos...
