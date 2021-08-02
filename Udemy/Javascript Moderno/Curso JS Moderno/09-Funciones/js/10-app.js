2546049e5778076fcc02597104c7d16fdf3e6cdd
DCueto <dcueto14.info@gmail.com>
2021-08-02 19:53:53 +0200

    Arrow functions

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/09-Funciones/js/10-app.js
@@ -0,0 +1,16 @@
+// Arrow functions
+
+// Se agregó en ECMAScript 6.
+// Se declaran como una function expression (como una variable)
+// Las arrow functions permiten simplificar el código
+// Cuando las arrow function tienen solo una línea, dan por implícito el "return"
+// Las llaves son opcionales. Se utilizan para múltiples lineas en una arrow function.
+
+const aprendiendo = function(){
+  console.log("Aprendiendo JS");
+}
+aprendiendo();
+
+// ARROW FUNCTION
+const aprendiendo2 = () => "Aprendiendo Arrow Functions";
+console.log(aprendiendo2());
