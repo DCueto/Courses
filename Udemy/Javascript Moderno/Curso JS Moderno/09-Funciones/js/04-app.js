870e8c958bd930b2d7b6e20b03401c06bba76f80
DCueto <dcueto14.info@gmail.com>
2021-08-02 18:30:03 +0200

    Diferencia de sintaxis de Función y Método

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/09-Funciones/js/04-app.js
@@ -0,0 +1,11 @@
+
+// Diferencia entre Función y Método
+
+const numero1 = 20;
+const numero2 = "20";
+
+console.log(parseInt(numero2)); // Esto es una función 
+// Sintaxis: nombre + paréntesis -> parseInt();
+
+console.log(numero1.toString()); // Esto es un método -> 
+// Sintaxis: objeto + . + nombre + paréntesis -> numero1.tostring(); 
