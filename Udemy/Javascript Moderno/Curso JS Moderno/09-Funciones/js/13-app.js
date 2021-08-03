a920d0db309a1d658104de4e85b894dbce0a008f
DCueto <dcueto14.info@gmail.com>
2021-08-03 09:12:45 +0200

    Uso de get y set

--- Udemy/Javascript Moderno/Curso JS Moderno/09-Funciones/js/13-app.js
+++ Udemy/Javascript Moderno/Curso JS Moderno/09-Funciones/js/13-app.js
@@ -1,12 +1,25 @@
 // Métodos de propiedad (MÉTODOS) usando ARROW FUNCTIONS
 
 const reproductor = {
+  cancion: '',
   reproducir: id => console.log(`Reproduciendo canción con el id ${id}`),
   pausar: () => console.log('pausando...'),
   borrar: id => console.log(`Borrando canción con id ${id}`),
   crearPlaylist: nombre => console.log(`Creando la playlist de ${nombre}`),
   reproducirPlaylist: nombre => console.log(`Reproduciendo playlist ${nombre}`),
+
+  set nuevaCancion(cancion){
+    this.cancion = cancion;
+    console.log(`Añadiendo ${cancion}`);
+  },
+
+  get obtenerCancion(){
+    console.log(`${this.cancion}`);
+  },
 }
+
+reproductor.nuevaCancion = 'Perspective - Wasted Penguinz';
+reproductor.obtenerCancion;
 
 reproductor.reproducir(30);
 reproductor.pausar();
