7c2d4094d3df4336ba61b3ac24c12650b2438460
DCueto <dcueto14.info@gmail.com>
2021-08-08 11:47:53 +0200

    Array Methods: .some y .includes

--- Udemy/Javascript Moderno/Curso JS Moderno/12-ArrayMethods/js/01-app.js
+++ Udemy/Javascript Moderno/Curso JS Moderno/12-ArrayMethods/js/01-app.js
@@ -1,3 +1,10 @@
+// Array Methods
+// Hacen todo el trabajo pesado por ti. Evitan tener que utilizar iteradores y automatizan procesos.
+// Es importante conocer como hacer todo con un iterador para entender correctamente el funcionamiento
+// de los array methods.
+
+
+
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
 const carrito = [
@@ -8,3 +15,40 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+
+// Comprobar si un arreglo contiene un valor en un item en concreto
+
+// Forma "manual"
+meses.forEach( mes => {
+  if(mes === 'Enero') {
+    console.log('Enero existe');
+  }
+});
+
+// Con .includes
+// .includes comprueba si existe un valor en un array y devuelve true o false.
+// Includes solo sirve para arrays con solo un valor basado en el índice
+// Como argumento se le pasa un valor
+const resultado = meses.includes('Enero');
+console.log(resultado);
+
+// Con .some
+// Para revisar si un valor existe en un array de objetos tenemos que usar otro array method.
+// Como argumento se le pasa una función
+// La función tiene que devolver un valor con un return.
+const existe = carrito.some( producto => producto.nombre === 'Celular');
+console.log(existe);
+
+// Ejemplo de .some para revisar si un producto cuesta más de 600€
+const alerta_precio = carrito.some( producto => {
+  return producto.precio > 600;
+});
+
+console.log(alerta_precio ? 'Hay un producto en tu carrito con un valor superior a 600€' : 'Todo OK');
+
+
+// Iterar un arreglo de índices con .some
+// Para este caso concreto es más sencillo usar el .includes
+const existe2 = meses.some(mes => mes === 'Febrero');
+console.log(existe2);
