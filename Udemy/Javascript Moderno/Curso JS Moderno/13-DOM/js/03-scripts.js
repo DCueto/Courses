76f2f7d7af2f4cd48ebdc98106f2f8bc80e012e0
DCueto <dcueto14.info@gmail.com>
2021-08-10 18:20:09 +0200

    Seleccionar un elemento HTML por su ID con .getElementById

---
+++ Udemy/Javascript Moderno/Curso JS Moderno/13-DOM/js/03-scripts.js
@@ -0,0 +1,10 @@
+// Seleccionar un elemento HTML por su id
+// getElementById
+
+// Nos devuelve directamente el elemento HTML
+const formulario = document.getElementById('formulario');
+console.log(formulario);
+
+// Si no existe, nos devuelve un NULL
+const noExiste = document.getElementById('no-existe');
+console.log(noExiste);
