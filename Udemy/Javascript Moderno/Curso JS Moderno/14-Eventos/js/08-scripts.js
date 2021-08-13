5851c471a18de398c795400f68aa880aff8a8325
DCueto <dcueto14.info@gmail.com>
2021-08-13 14:04:25 +0200

    Prevenir el Event Bubbling a la hora de generar contenido con una función en el evento

--- Udemy/Javascript Moderno/Curso JS Moderno/14-Eventos/js/08-scripts.js
+++ Udemy/Javascript Moderno/Curso JS Moderno/14-Eventos/js/08-scripts.js
@@ -1,4 +1,19 @@
 // Evitar la propagación con contenido creado...
+
+// Esta técnica es para cuando el código es generado.
+// Es muy común generar el contenido de esta forma cuando consumes datos de una API
+
+// CREAMOS LA FUNCIÓN PARA EL EVENTO
+// el problema es que si queremos pasarle un parámetro a la función, al tener que añadir los paréntesis
+// en el elemento.onclick, se ejecutará al momento en vez de cuando le demos click.
+function nuevaFuncion(id){
+  console.log('desde nueva función', id);
+}
+
+// la solución sería aplicarle una función anónima al onlick del elemento con la función
+// nuevaFuncion() dentro de esa función anónima.
+
+
 const parrafo1 = document.createElement('P');
 parrafo1.textContent = 'Concierto';
 parrafo1.classList.add('categoria');
@@ -9,10 +24,20 @@ const parrafo2 = document.createElement('P');
 parrafo2.textContent = 'Concierto de Rock';
 parrafo2.classList.add('titulo');
 
+// APLICAMOS UNA FUNCIÓN ANÓNIMA AL EVENTO ONCLICK CON LA FUNCIÓN nuevaFuncion()
+// BIEN -> Se ejecuta correctamente a la hora de disparar el evento
+parrafo2.onclick = function(){
+  nuevaFuncion(1);
+}
+
 // 3er parrafo...
 const parrafo3 = document.createElement('p');
 parrafo3.textContent = '$800 por persona';
 parrafo3.classList.add('precio');
+
+// APLICAMOS UNA FUNCIÓN AL EVENTO ONCLICK
+// MAL -> Se ejecuta solo al cargarse el documento, no con el evento.
+parrafo3.onclick = nuevaFuncion(1);
 
 // crear el div...
 const info = document.createElement('div');
