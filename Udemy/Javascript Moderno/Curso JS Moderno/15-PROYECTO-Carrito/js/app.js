cb1ea741e8a95ece8736b21845c8ea2ded16e1dc
DCueto <dcueto14.info@gmail.com>
2021-08-14 00:49:04 +0200

    Extraer informacion del curso seleccionado

--- Udemy/Javascript Moderno/Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
+++ Udemy/Javascript Moderno/Curso JS Moderno/15-PROYECTO-Carrito/js/app.js
@@ -21,7 +21,28 @@ function agregarCurso(e){
 
   // Si pulso al botón "agregar-carrito" -> ejecutar
   if(e.target.classList.contains('agregar-carrito')){
-    console.log(e.target.parentElement.children[0]);
+    const cursoSeleccionado = e.target.parentElement.parentElement;
+    console.log(e);
+
+    leerDatosCurso(cursoSeleccionado);
   }
 
+}
+
+// Lee el contenido del HTML al que le dimos click y extrae información del curso
+function leerDatosCurso(curso){
+  // curso referencia al elemento .card seleccionado en el evento
+  console.log(curso);
+
+  // Crear un objeto con el contenido del curso actual
+  const infoCurso = {
+    imagen: curso.querySelector('img').src,
+    titulo: curso.querySelector('h4').textContent,
+    autor: curso.querySelector('p').textContent,
+    precio: curso.querySelector('p.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id'),
+    cantidad: 1,
+  }
+
+  console.log(infoCurso);
 }
