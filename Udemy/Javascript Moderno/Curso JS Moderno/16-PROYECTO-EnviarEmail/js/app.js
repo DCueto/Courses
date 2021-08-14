761ba5f664b7a6cb4f4b350390f73a54cb11656b
DCueto <dcueto14.info@gmail.com>
2021-08-14 20:29:51 +0200

    Validar campos

--- Udemy/Javascript Moderno/Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Udemy/Javascript Moderno/Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -5,17 +5,26 @@
 const btnEnviar = document.querySelector('#enviar');
 const btnReset = document.querySelector('#resetBtn');
 
-
+// Variables campos
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 
 
 // EVENTOS
 
 eventListeners();
 function eventListeners(){
+
+  // Cuando la app arranca
   document.addEventListener('DOMContentLoaded', iniciarApp);
+
+  // Campos de formulario
+  email.addEventListener('blur', validarFormulario);
+  asunto.addEventListener('blur', validarFormulario);
+  mensaje.addEventListener('blur', validarFormulario);
+
 }
-
-
 
 
 
@@ -23,7 +32,20 @@ function eventListeners(){
 
 
 function iniciarApp(){
-  console.log('iniciando...');
   btnEnviar.disabled = true;
+  // CSS Tailwind - Estilos para el boton enviar bloqueado
   btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+
+function validarFormulario(e){
+
+  if(e.target.value.length > 0){
+    // Hay contenido
+    console.log(e.target.value);
+    e.target.classList.remove('border', 'border-red-500');
+  } else{
+    // No hay ningún contenido
+    e.target.classList.add('border', 'border-red-500');
+  }
 }
