842efec1c9ed6b70ea5bb72ac53e6d5b7594a354
DCueto <dcueto14.info@gmail.com>
2021-08-18 19:30:22 +0200

    Filtrar autos por Marca

--- Udemy/Javascript Moderno/Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
+++ Udemy/Javascript Moderno/Curso JS Moderno/17-PROYECTO-Buscador/js/app.js
@@ -1,6 +1,9 @@
 
 // VARIABLES
 
+  const buscador = document.querySelector('#buscador');
+  const selects = document.querySelectorAll('#buscador select');
+
   // Contenedor de resultados
   const resultado = document.querySelector('#resultado');
 
@@ -43,21 +46,31 @@ document.addEventListener('DOMContentLoaded', ()=>{
   llenarSelect();
 });
 
-// Selects de búsqueda
-// Con el evento 'change', se ejecuta al cambiar de opción en un select
-selectMarca.addEventListener('change', e => datosBusqueda.marca = e.target.value);
-selectYear.addEventListener('change', e => datosBusqueda.year = e.target.value);
-selectMinimo.addEventListener('change', e => datosBusqueda.minimo = e.target.value);
-selectMaximo.addEventListener('change', e => datosBusqueda.maximo = e.target.value);
-selectPuertas.addEventListener('change', e => datosBusqueda.puertas = e.target.value);
-selectTransmision.addEventListener('change', e => datosBusqueda.transmision = e.target.value);
-selectColor.addEventListener('change', e => datosBusqueda.color = e.target.value);
+  // Selects de búsqueda
+  // Con el evento 'change', se ejecuta al cambiar de opción en un select
+  selectMarca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value;
+    filtrarAuto();
+  });
+  selectYear.addEventListener('change', e => datosBusqueda.year = e.target.value);
+  selectMinimo.addEventListener('change', e => datosBusqueda.minimo = e.target.value);
+  selectMaximo.addEventListener('change', e => datosBusqueda.maximo = e.target.value);
+  selectPuertas.addEventListener('change', e => datosBusqueda.puertas = e.target.value);
+  selectTransmision.addEventListener('change', e => datosBusqueda.transmision = e.target.value);
+  selectColor.addEventListener('change', e => datosBusqueda.color = e.target.value);
 
 // Imprimir datosBusqueda con botón Datos
 btnDatos.addEventListener('click', e => {
   e.preventDefault();
   console.log(datosBusqueda);
+
+  limpiarHTML();
 });
+
+// Imprimir datos de búsqueda
+// selects.forEach( select => {
+//   select.addEventListener('change', filtrarAuto);
+// });
 
 // FUNCIONES
 
@@ -89,5 +102,59 @@ function llenarSelect(){
     yearHTML.textContent = i;
 
     selectYear.appendChild(yearHTML);
+  }
+}
+
+
+// Función que filtra en base a la búsqueda
+
+function filtrarAuto(){
+  const resultado = autos.filter(filtrarMarca);
+  console.log(resultado);
+}
+
+// Filtro select marca
+function filtrarMarca(auto){
+  const {marca} = datosBusqueda;
+  if(marca){
+    return auto.marca === marca;
+  }
+  return auto;
+}
+
+// Imprime los coches con el filtro de los datosBusqueda
+// function imprimirBusqueda(e){
+//   limpiarHTML();
+//   console.log(e);
+//   const id = e.target.id;
+//   const value = e.target.value;
+//   if(id == 'marca'){
+//     const busqueda = autos.filter(auto => auto.marca == value);
+//     console.log(busqueda);
+//     busquedaHTML(busqueda);
+//   }
+// }
+
+// function busquedaHTML(busqueda){
+//   busqueda.forEach(auto => {
+//     // destructoring de auto
+//     const {marca, modelo, year, precio, puertas, color, transmision} = auto;
+
+//     // creamos un párrafo por cada coche
+//     const autoHTML = document.createElement('p');
+
+//     autoHTML.textContent = `
+//       ${marca} ${modelo} - ${year} - ${puertas} puertas - Color ${color} - Transmisión ${transmision} : ${precio}€
+//     `;
+
+//     // Insertar en el HTML
+//     resultado.appendChild(autoHTML);
+//   });
+// }
+
+// Limpiar la sección #resultado
+function limpiarHTML(){
+  while(resultado.firstChild){
+    resultado.removeChild(resultado.firstChild);
   }
 }
