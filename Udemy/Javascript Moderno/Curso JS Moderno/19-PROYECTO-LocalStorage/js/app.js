792f2e92dc27785bcc51e3f665b27a7a1b4e2063
DCueto <dcueto14.info@gmail.com>
2021-08-25 20:15:33 +0200

    Mostrar Tweets al HTML

--- Udemy/Javascript Moderno/Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
+++ Udemy/Javascript Moderno/Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
@@ -6,7 +6,7 @@ let tweets = [];
 
   // Selectores DOM
   const form = document.querySelector('#formulario');
-  const tweet = document.querySelector('#formulario #tweet');
+  const tweetInput = document.querySelector('#formulario #tweet');
   const agregarBtn = document.querySelector('#formulario input[type="submit"]');
   const contenedorTweets = document.querySelector('#lista-tweets');
 
@@ -23,7 +23,7 @@ function eventListeners(){
 // FUNCIONES
 
 function initApp(){
-  console.log('Iniciando App...');
+  console.log('DOM cargado correctamente');
   eventListeners();
 }
 
@@ -31,20 +31,36 @@ function agregarTweet(e){
   e.preventDefault();
   
   // Validar el textarea
-  if(tweet.value){
+  if(tweetInput.value){
     if(form.querySelector('.error')){
       form.querySelector('.error').remove();
     }
-    console.log('Agregando Tweet...');
   } else{
     mostrarError('Un mensaje no puede ir vacío');
     return; // Evita que se ejecute más líneas de código
   }
 
+  // Añadir tweet
   console.log('Agregando Tweet...');
+
+  const tweet = {
+    id: Date.now(),
+    tweet: tweetInput.value,
+  }
+
+  // Añadir al arreglo de tweets
+  tweets = [...tweets, tweet];
+  console.log(tweets);
+
+  // Imprimir el arreglo de tweets en HTML
+  renderHTML();
+
+  // Reiniciar el textarea
+  form.reset();
+
 }
 
-
+// Mostrar un error
 function mostrarError(mensaje){
 
   const errorHTML = document.createElement('p');
@@ -59,4 +75,27 @@ function mostrarError(mensaje){
   setTimeout(()=>{
     errorHTML.remove();
   }, 3000);
+}
+
+// Mostrar tweets en el contenedor HTML
+function renderHTML(){
+  if (tweets.length > 0){
+    limpiarHTML();
+    tweets.forEach( tweet => {
+      // Crear HTML
+      const li = document.createElement('li');
+      li.classList.add('tweet');
+      li.textContent = tweet.tweet;
+
+      // Insertar
+      contenedorTweets.appendChild(li);
+    });
+  }
+}
+
+// Limpiar el contenedor HTML de tweets
+function limpiarHTML(){
+  while(contenedorTweets.firstChild){
+    contenedorTweets.removeChild(contenedorTweets.firstChild);
+  }
 }
