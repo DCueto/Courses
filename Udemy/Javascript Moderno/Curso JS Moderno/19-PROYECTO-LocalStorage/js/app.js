1f76972a9d0d6a4d10dc032451e24e0f2f96077e
DCueto <dcueto14.info@gmail.com>
2021-08-25 19:37:16 +0200

    Validando el formulario

--- Udemy/Javascript Moderno/Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
+++ Udemy/Javascript Moderno/Curso JS Moderno/19-PROYECTO-LocalStorage/js/app.js
@@ -5,6 +5,7 @@
 let tweets = [];
 
   // Selectores DOM
+  const form = document.querySelector('#formulario');
   const tweet = document.querySelector('#formulario #tweet');
   const agregarBtn = document.querySelector('#formulario input[type="submit"]');
   const contenedorTweets = document.querySelector('#lista-tweets');
@@ -28,9 +29,34 @@ function initApp(){
 
 function agregarTweet(e){
   e.preventDefault();
+  
+  // Validar el textarea
+  if(tweet.value){
+    if(form.querySelector('.error')){
+      form.querySelector('.error').remove();
+    }
+    console.log('Agregando Tweet...');
+  } else{
+    mostrarError('Un mensaje no puede ir vacío');
+    return; // Evita que se ejecute más líneas de código
+  }
 
   console.log('Agregando Tweet...');
-  
-  // console.log(e.target.parentElement.children[1].value);
-  console.log(tweet.value);
+}
+
+
+function mostrarError(mensaje){
+
+  const errorHTML = document.createElement('p');
+  errorHTML.textContent = mensaje;
+  errorHTML.classList.add('error');
+
+  if(form.querySelector('.error')){
+    form.querySelector('.error').remove();
+  }
+
+  form.appendChild(errorHTML);
+  setTimeout(()=>{
+    errorHTML.remove();
+  }, 3000);
 }
